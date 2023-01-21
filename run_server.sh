384325df7eea9e27c1e2959880ae409bcfca29c0
hideckies <hideckies@gmail.com>
2023-01-21 22:31:01 +0900

    updated run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -7,19 +7,8 @@ PATH_PREDICTIONS_CSV=$DIR_CURRENT/identifier/model/predictions.csv
 # Retrieve hash types
 HASH_TYPES=$(head -n 1 $PATH_PREDICTIONS_CSV)
 
-echo -e "Update the list of the \"classes\" variable in docs/index.html as below:\n"
-echo -n "const classes = ["
-IFS=', ' read -r -a array <<< $HASH_TYPES
-for i in "${!array[@]}"; do
-    echo -n \"${array[$i]}\"
-    if [ ${array[$i]} != ${array[-1]} ]; then
-        echo -n ", "
-    fi
-done
-echo -n "];"
-
-echo -e "\n\nAlso update the description:\n"
-echo -e "Guessable hases are $HASH_TYPES.\n"
+echo -e "Below is dectable hashes. Update the list of the \"classes\" variable in docs/index.html as below:\n"
+python3 classes_to_array.py "$HASH_TYPES"
 
 # Start local server
 echo -e "\n\nStart local server."
